020bdd0228957b261b570bc9df69265df16d3fba
Steven McSorley <smcsorl@sky.com>
2025-11-30 21:32:52 +0000

    Add missing using statement for ILogger

--- MyDockerProject/MyDockerProject/Controllers/Api/BookingsController.cs
+++ MyDockerProject/MyDockerProject/Controllers/Api/BookingsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using MyDockerProject.Data;
 using MyDockerProject.Models;
 using MyDockerProject.Services;
